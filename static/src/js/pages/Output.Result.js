1f529b15c1fdeb2909f2e96dda2f6c07092205c5
Cloud9c <kire5030@gmail.com>
2023-04-04 11:22:31 -0400

    Change to "download data as a csv"

--- static/src/js/pages/Output.Result.js
+++ static/src/js/pages/Output.Result.js
@@ -167,7 +167,7 @@ class OutputResult {
 		downloadCSV.setAttribute('download', title + '.csv');
 		downloadCSV.setAttribute('href', encodedUri);
 		downloadCSV.classList.add('modebar-btn');
-		downloadCSV.dataset.title = 'Download plot as a csv';
+		downloadCSV.dataset.title = 'Download data as a csv';
 		downloadCSV.dataset.toggle = 'false';
 		downloadCSV.dataset.gravity = 'n';
 		downloadCSV.innerHTML = '<svg width="16" height="16" viewBox="0 0 16 16" class="icon"><path d="M8 9l4-4h-3v-4h-2v4h-3zM11.636 7.364l-1.121 1.121 4.064 1.515-6.579 2.453-6.579-2.453 4.064-1.515-1.121-1.121-4.364 1.636v4l8 3 8-3v-4z"/></svg>';
